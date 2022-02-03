4cbad74f68ff594990a88d992b9146ec0fff12cb
blucky8649 <blucky8649@gmail.com>
2022-02-03 23:36:09 +0900

    숫자 문자열과 영단어 solved

---
+++ src/main/kotlin/Kakao_Internship_2021/숫자_문자열과_영단어/Solution.kt
@@ -0,0 +1,12 @@
+package Kakao_Internship_2021.숫자_문자열과_영단어
+
+class Solution {
+    fun solution(s: String): Int {
+        val nums = arrayOf("zero", "one", "two", "three", "four", "five", "six", "seven","eight", "nine")
+        var str = s
+        for (i in 0 until nums.size) {
+            str = str.replace(nums[i], i.toString())
+        }
+        return str.toInt()
+    }
+}
